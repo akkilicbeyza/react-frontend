a33860cf59e5bed6536191039aabee673c618419
srptas <seraptas@yandex.com>
2023-07-16 01:23:58 +0300

    create account is done

--- src/App.js
+++ src/App.js
@@ -5,80 +5,79 @@ import Form from './components/form/Form';
 import { useState } from 'react';
 import axios from 'axios';
 
+
 function App() {
-  const [user, setUser] = useState({name: "", password: ""})
+
+  
+  const [user, setUser] = useState({name: "", password: "", email: ""})
   const handleChange = (e) => {
     if(e.target.type === "text"){
     setUser({...user, name:e.target.value})
     console.log(user);
     }
-    else {
+    else if(e.target.type === "password") {
       setUser({...user, password:e.target.value})
       console.log(user);
     }
-  };
-  const handleClick = () => {
-    if(user.name === ""){
-      alert("user name cannot be empty");
+    else {
+      setUser({...user, email:e.target.value})
     }
-    else if(user.password === ""){
-      alert("password cannot be empty");
-    }
-    else{
-      alert("user name: " + user.name + " password: " + user.password);
-    }
+   
+};
 
-
-    const handleSave = (e) => {
-      e.preventDefault();
-
-      const url = 'https://localhost:44368/Auth/Authenticate';
-      const data = {
-        UserName : user.name,
-        Password : user.password
-      }
+ 
   
-      axios.post(url, data)
-      .then((result) => {
-        const dt = result.data;
-        })
-      .catch((error) => {
-        console.log(error)
-      })
+const handleSave = (e) => {
+  e.preventDefault();
+
+  if(user.name === ""){
+    alert("username cannot be empty");
+  }
+  else if(user.password === ""){
+    alert("password cannot be empty");
+  }
+  else{
+    alert("username: " + user.name + " password: " + user.password);
+  }
+
+
+
+  const url = 'https://localhost:44368/api/User/registration';
+  const data = {
+    Email : user.email,
+    UserName : user.name,
+    Password: user.password
+  }
+
+  axios.post(url,data)
+
+  .then((result) => {
+    const dt = result.data;
+  })
+  .catch((error) => {
+    if (error.response) {
+      console.log('Server responded with status code:', error.response.status);
+      console.log('Response data:', error.response.data);
+    } else if (error.request) {
+      console.log('No response received:', error.request);
+    } else {
+      console.log('Error creating request:', error.message);
     }
+  })
 
+}
 
-    const handleSave = (e) => {
-      e.preventDefault();
-
-      const url = 'https://localhost:44368/api/User/registration';
-      const data = {
-        UserName : user.namme,
-        Password: user.passwordd
-      }
-
-      axios.post(url,data)
-      .then((result) => {
-
-        const dt = result.data;
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-
-    }
-
-
-  };
   return (
     <div className="App">
       <h2>WELCOME!</h2>  
       <Form>
-        <Input type={"text"} value={user.name} placeHolder={"user name"} onChange={handleChange} />
-        <Input type={"password"} value={user.password} placeHolder={"password"} onChange={handleChange} />
-        <Button onClick={handleClick} />
+        <Input type={"email"} value={user.email} placeHolder={"E-mail"} onChange={handleChange} />
+        <Input type={"text"} value={user.name} placeHolder={"Username"} onChange={handleChange} />
+        <Input type={"password"} value={user.password} placeHolder={"Password"} onChange={handleChange} />
+        <Button onClick={(e) => handleSave(e)} />
       </Form>
     </div>
+    
   );
 }
 
--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import "./button.css"
 
+
+
+  
 function Button({onClick}) {
   return (
-        <button className="buton" onClick={onClick}>Log In</button>
+        <button className="buton" onClick={onClick}>Create Account</button>
   )
 }
 
